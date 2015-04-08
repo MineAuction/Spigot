ea06373f9d6c6af66325d5e872b0365cd1dd60fd
Sognus <msognus@gmail.com>
2015-04-08 19:43:01 +0200

    Version a.1.0-B12

--- src/main/java/cz/sognus/mineauction/WebInventoryMeta.java
+++ src/main/java/cz/sognus/mineauction/WebInventoryMeta.java
@@ -104,7 +104,8 @@ public class WebInventoryMeta
 		if(mapMeta.containsKey("repair-cost"));
 		{
 			Bukkit.broadcastMessage((String) mapMeta.get("repair-cost"));
-			Integer integer = Integer.valueOf(String.valueOf(mapMeta.get("repair-cost")));
+	        Double d = new Double(Double.valueOf((String)mapMeta.get("repair-cost")));
+	        Integer i = d.intValue();
 			mapMeta.replace("repair-cost", integer);
 		}
 	
