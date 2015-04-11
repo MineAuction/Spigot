15d433099dc67e74a20bd5eec7f8ec2a9bc90d98
Luboš Hubáček <seki@lostwar.cz>
2015-04-11 23:52:37 +0200

    Version a.1.0-B37

--- src/main/java/cz/sognus/mineauction/WebInventoryMeta.java
+++ src/main/java/cz/sognus/mineauction/WebInventoryMeta.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,177 +22,176 @@ import com.google.gson.JsonSerializer;
 import com.google.gson.LongSerializationPolicy;
 import com.google.gson.reflect.TypeToken;
 
-public class WebInventoryMeta
-{
+public class WebInventoryMeta {
 	private final ItemStack item;
-	
-	public WebInventoryMeta(ItemStack i)
-	{
+
+	public WebInventoryMeta(ItemStack i) {
 		this.item = i;
 	}
-	
-	
+
 	// get number of item in stack
-	public int getItemQty()
-	{
-		if(this.item == null) return 0;
-		return this.item.getAmount();		
+	public int getItemQty() {
+		if (this.item == null)
+			return 0;
+		return this.item.getAmount();
 	}
-	
-	public String getLore()
-	{
-		if(this.item == null || !this.item.getItemMeta().hasLore()) return "";
+
+	public String getLore() {
+		if (this.item == null || !this.item.getItemMeta().hasLore())
+			return "";
 		List<String> loreList = this.item.getItemMeta().getLore();
-		if(loreList == null) return "";
+		if (loreList == null)
+			return "";
 		Gson gson = new Gson();
 		return gson.toJson(loreList);
 	}
-	 
+
 	@SuppressWarnings("unchecked")
-	public List<String> getLoreList(String json)
-	{
-		if(json == "" || json == null) return null;
+	public List<String> getLoreList(String json) {
+		if (json == "" || json == null)
+			return null;
 		Gson gson = new Gson();
 		List<String> loreList = new ArrayList<String>();
-		Type type = new TypeToken<List<String>>(){}.getType();
-		loreList = (List<String>)gson.fromJson(json,type);
+		Type type = new TypeToken<List<String>>() {
+		}.getType();
+		loreList = (List<String>) gson.fromJson(json, type);
 		return loreList;
-		
+
 	}
-	
+
 	// get item metadata in json format
-	public String getItemMeta()
-	{
-		if(this.item == null) return "";
+	public String getItemMeta() {
+		if (this.item == null)
+			return "";
 		Map<String, Object> mapMeta = this.item.getItemMeta().serialize();
 
-		//setup gson
+		// setup gson
 		// Gson setup
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(Double.class,  new JsonSerializer<Double>() {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.registerTypeAdapter(Double.class,
+				new JsonSerializer<Double>() {
 
-            public JsonElement serialize(Double src, Type typeOfSrc,
-                        JsonSerializationContext context) {
-                    Integer value = (int)Math.round(src);
-                    return new JsonPrimitive(value);
-                }
-            });
+					public JsonElement serialize(Double src, Type typeOfSrc,
+							JsonSerializationContext context) {
+						Integer value = (int) Math.round(src);
+						return new JsonPrimitive(value);
+					}
+				});
 
-        Gson gson = gsonBuilder.setLongSerializationPolicy(LongSerializationPolicy.STRING).create();
-		
+		Gson gson = gsonBuilder.setLongSerializationPolicy(
+				LongSerializationPolicy.STRING).create();
+
 		// HashMap to json
-        String json = gson.toJson(mapMeta);
-		
+		String json = gson.toJson(mapMeta);
+
 		return json;
-			
+
 	}
-	
+
 	// get item enchantments in json format
-	public String getItemEnchantments()
-	{
-		if(this.item == null) return "";
-		
+	public String getItemEnchantments() {
+		if (this.item == null)
+			return "";
+
 		Map<Enchantment, Integer> mapEnchantment = this.item.getEnchantments();
 		Map<String, Object> mapData = new HashMap<String, Object>();
-		
-		for(Map.Entry<Enchantment, Integer> entry : mapEnchantment.entrySet())
-		{
+
+		for (Map.Entry<Enchantment, Integer> entry : mapEnchantment.entrySet()) {
 			String s = entry.getKey().getName();
 			Object o = entry.getValue();
-			
+
 			mapData.put(s, o);
-			
+
 		}
-		
-		Gson gson = new GsonBuilder().setLongSerializationPolicy(LongSerializationPolicy.STRING).create();
+
+		Gson gson = new GsonBuilder().setLongSerializationPolicy(
+				LongSerializationPolicy.STRING).create();
 		String json = gson.toJson(mapData);
-		
+
 		return json;
 	}
-	
+
 	// get metadata Hashmap from json
 	@SuppressWarnings("unchecked")
-	public static Map<String, Object> getItemMetaMap(String Ijson)
-	{
+	public static Map<String, Object> getItemMetaMap(String Ijson){
 		if(Ijson == "" || Ijson == null) return null;
-		
-		// Try to fix json
-        Pattern ptrn = Pattern.compile("\\\"repair-cost\\\":[0-9]*");
-        Matcher m=ptrn.matcher(Ijson);
-        if(m.find()) Ijson.replaceFirst("\\\"repair-cost\\\":[0-9]*\\.[0-9]", m.group());
-		
-		// Gson setup
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(Double.class,  new JsonSerializer<Double>() {
 
-            public JsonElement serialize(Double src, Type typeOfSrc,
-                        JsonSerializationContext context) {
-                    Integer value = (int)Math.round(src);
-                    return new JsonPrimitive(value);
-                }
-            });
-
-        Gson gson = gsonBuilder.setLongSerializationPolicy(LongSerializationPolicy.STRING).create();
-		 
-		// Json to HashMap convert
-		Map<String, Object> mapMeta = new HashMap<String, Object>();
-		mapMeta = (Map<String, Object>) gson.fromJson(Ijson, mapMeta.getClass());
-
-		return mapMeta;
+        // Json to HashMap convert
+        Map<String, Object> mapMeta = new HashMap<String, Object>();
+        mapMeta = (Map<String, Object>) gson.fromJson(Ijson, mapMeta.getClass());
+        
+        // fix hash map by Sekiphp
+        String property = "repair-cost";
+		for (Entry<String, Object> entry : mapMeta.entrySet()){
+			if(entry.getKey().equalsIgnoreCase(property)){
+				String fixed = String.valueOf(entry.getValue()).replaceAll("\\.\\d+", "");
+				
+				mapMeta.remove(entry.getKey());
+				mapMeta.put(property, new Integer(fixed));
+				break;
+			}
+		}  
 		
-		
+		// check the hashMap
+		for (Entry<String, Object> entry : mapMeta.entrySet()){
+			Bukkit.broadcastMessage("key: " + entry.getKey() + ", value: " + entry.getValue() + ", type: " + entry.getValue().getClass().getName());
+		}
+        
+
+        return mapMeta;		
 	}
-	
+
 	// get enchantments hashmap from json
 	@SuppressWarnings("unchecked")
-	public static Map<Enchantment, Integer> getItemEnchantmentMap(String Ijson)
-	{
-		if(Ijson == "" || Ijson == null) return null;
-		
+	public static Map<Enchantment, Integer> getItemEnchantmentMap(String Ijson) {
+		if (Ijson == "" || Ijson == null)
+			return null;
+
 		String json = Ijson.replaceAll("\\.\\d+", "");
-		
-		Gson gson = new GsonBuilder().setLongSerializationPolicy(LongSerializationPolicy.STRING).create();
+
+		Gson gson = new GsonBuilder().setLongSerializationPolicy(
+				LongSerializationPolicy.STRING).create();
 		Map<String, Object> mapData = new HashMap<String, Object>();
-		Type type = new TypeToken<Map<String, String>>(){}.getType();
-		mapData = (Map<String, Object>)gson.fromJson(json,type);
-		
+		Type type = new TypeToken<Map<String, String>>() {
+		}.getType();
+		mapData = (Map<String, Object>) gson.fromJson(json, type);
+
 		Map<Enchantment, Integer> mapEnchant = new HashMap<Enchantment, Integer>();
-		
-		for(Map.Entry<String, Object> entry : mapData.entrySet())
-		{
-			Bukkit.broadcastMessage("EnchMapDebg: "+entry.getKey()+"=>"+entry.getValue());
-			
+
+		for (Map.Entry<String, Object> entry : mapData.entrySet()) {
+			Bukkit.broadcastMessage("EnchMapDebg: " + entry.getKey() + "=>"
+					+ entry.getValue());
+
 			Enchantment e = Enchantment.getByName(entry.getKey());
 			Integer i = (Integer) Integer.parseInt((String) entry.getValue());
-			
+
 			mapEnchant.put(e, i);
 		}
-		
+
 		return mapEnchant;
 	}
-	
+
 	// get item id (string)
 	@SuppressWarnings("deprecation")
-	public int getId()
-	{
-		if(this.item == null) return -1;
-		
+	public int getId() {
+		if (this.item == null)
+			return -1;
+
 		int itemTypeName = item.getType().getId();
-		
+
 		return itemTypeName;
 	}
-	
+
 	// get item durability
-	public short getDurability()
-	{
-		if(this.item == null) return -1;
-		
+	public short getDurability() {
+		if (this.item == null)
+			return -1;
+
 		short int16 = item.getDurability();
 		return int16;
 	}
-	
-	public ItemStack getItemStack()
-	{
+
+	public ItemStack getItemStack() {
 		return this.item;
 	}
 }
